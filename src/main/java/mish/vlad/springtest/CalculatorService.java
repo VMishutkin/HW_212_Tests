5fde1598563b118c84b84258cc552596569485d7
VMishutkin <vladmish@list.ru>
2022-08-24 12:22:19 +0700

    Выполнил ДЗ, написал проверку на ввод

---
+++ src/main/java/mish/vlad/springtest/CalculatorService.java
@@ -0,0 +1,13 @@
+package mish.vlad.springtest;
+
+public interface CalculatorService {
+    public String plus(String num1, String num2);
+
+    public String minus(String num1, String num2);
+
+    public String multiply(String num1, String num2);
+
+    public String divide(String num1, String num2);
+
+    String greetings();
+}
---
+++ src/main/java/mish/vlad/springtest/CalculatorServiceImpl.java
@@ -0,0 +1,72 @@
+package mish.vlad.springtest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService{
+
+    @Override
+    public String greetings() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @Override
+    public String plus(String num1, String num2) {
+        if(num1==null || num2 == null){
+            return "Введите параметры num1 и num2";
+        }
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            return "Параметры должны быть целым числом";
+        }
+        return num1 + " + " + num2 + " = " + (Integer.parseInt(num1)+ Integer.parseInt(num2));
+    }
+
+
+
+    @Override
+    public String minus(String num1, String num2) {
+        if(num1==null || num2 == null){
+            return "Введите параметры num1 и num2";
+        }
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            return "Параметры должны быть целым числом";
+        }
+        return num1 + " - " + num2 + " = " + (Integer.parseInt(num1)-Integer.parseInt(num2));
+    }
+
+    @Override
+    public String multiply(String num1, String num2) {
+        if(num1==null || num2 == null){
+            return "Введите параметры num1 и num2";
+        }
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            return "Параметры должны быть целым числом";
+        }
+        return num1 + " * " + num2 + " = " + (Integer.parseInt(num1)*Integer.parseInt(num2));
+    }
+
+    @Override
+    public String divide(String num1, String num2) {
+        if(num1==null || num2 == null){
+            return "Введите параметры num1 и num2";
+        }
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            return "Параметры должны быть целым числом";
+        }
+        if(Integer.parseInt(num2)==0){
+            return "Деление на 0";
+        }
+        return num1 + " / " + num2 + " = " + (Integer.parseInt(num1)/ Integer.parseInt(num2));
+
+    }
+
+
+    private boolean isNumeric(String input) {
+        try{
+            Integer.parseInt(input);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
+    }
+}
